a2c48135b5920b45d4820f449b5be335e6bd8eb1
binksql4 <rayyt2551@gmail.com>
2025-05-12 15:53:04 +0700

    fixed hours return 0

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var x = setInterval(function () {
     var distance = C_Date - now;
 
     var day = Math.floor(distance / (1000*60*60*24));
-    var hour = Math.floor((distance % (1000*60*60*24)) / (1000*60*60*24));
+    var hour = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
     var minute = Math.floor((distance % (1000*60*60)) / (1000*60));
     var second = Math.floor((distance % (1000*60)) / (1000));
     var millisecond = Math.floor((distance % 1000) / 10);
